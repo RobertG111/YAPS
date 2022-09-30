a6b6a6ab598226a5d1d11051f52077b528f7e795
RobertG111 <33911814+RobertG111@users.noreply.github.com>
2022-09-30 14:26:42 +0100

    base complete

--- yaps.py
+++ yaps.py
@@ -10,23 +10,26 @@ parser = argparse.ArgumentParser()
 site = parser.add_argument("-s", "--site", help="Site", required=True)
 data = parser.add_argument("-d", "--data", help="Data", required=True)
 cookie = parser.add_argument("-c", "--cookie", help="Cookie <name=data>", required=False, default=None)
-user_agent = parser.add_argument("-u", "--user-agent", help="User-Agent", required=False, default="")
-header = parser.add_argument("-H", "--header", help="Header <name=data>", required=False, default=None)
+header = parser.add_argument("-H", "--header", help="Header Data", required=False, default=None)
+timeout = parser.add_argument("-t", "--timeout", help="Timeout", required=False, default=1)
 
 # Read arguments from command line
 args = parser.parse_args()
 
+# Variables
+postArray = []
+
 try:
     # Print Banner
     print('''
-    ________________________________
-    YAPS - Yet Another Phishing Site
-    --------------------------------
-            \   ^__^ 
-            \  (oo)\_______
-                (__)\       )\/
-                    ||----w |
-                    ||     ||''')
+  ________________________________
+  YAPS - Yet Another Phishing Site
+  --------------------------------
+         \   ^__^ 
+          \  (oo)\_______
+             (__)\       )\/
+                 ||----w |
+                 ||     ||''')
     
     
     # Check if site is up
@@ -40,37 +43,63 @@ try:
     # Find all post parameters
     for i in range(postCount):
         postArray[i] = postArray[i].split("=")
-        
+
     # Check if cookie is set
-    if args.cookie:
+    if args.cookie != None:
         cookieArray = args.cookie.split("=")
-        cookie = {cookieArray[0]: cookieArray[1]}
-                
-    # Check if user-agent is set
-    if args.user_agent:
-        user_agent = args.user_agent 
-                
+        args.cookie = {cookieArray[0]: cookieArray[1]}
+                    
     # Check if header is set
-    if args.header:
+    if args.header != None:
         headerArray = args.header.split("=")
-        header = {headerArray[0]: headerArray[1]}
+        args.header = {headerArray[0]: headerArray[1]}
+            
+    # Check if timeout is set
+    if args.timeout <= 1: 
+        timeout = args.timeout
 
     # Input number of files based on number of post parameters
     for i in range(postCount):
-        userInput = input("Enter target file for " + postArray[i][0] + ": ")
+        userInput = input("Enter target file for " + postArray[i][0] + " parameter: ")
         # Check if file exists
         if os.path.isfile(userInput):
             # Check if file is empty
             if os.stat(userInput).st_size == 0:
                 print("File is empty")
                 exit(1)
-            postArray[i].append(userInput)
+            # Add file to array
+            file = open(userInput, "r")
+            postArray[i][1] = file
         else:
             print("File not found. Exiting...")
             exit(1)
+ 
+    # Send requests
+    def sendRequest(data):
+        print("Sending request... " + str(data))
+        
+        # Default
+        if(args.cookie == None and args.header == None):   
+            requests.post(args.site, data=data, timeout=timeout)
+        elif(args.cookie != None and args.header == None):   
+            requests.post(args.site, data=data, cookies=cookie, timeout=timeout)
+        elif(args.cookie == None and args.header != None):        
+            requests.post(args.site, data=data, headers=header, timeout=timeout)
+        elif(args.cookie != None and args.header != None):
+            requests.post(args.site, data=data, cookies=cookie, headers=header, timeout=timeout)    
     
-    
-    print(postArray)
+    # Reading files
+    while True:
+        localDict = {} 
+        
+        for i in range(postCount):
+            line = postArray[i][1].readline().strip()
+            localDict[postArray[i][0]] = line
+            if line == "":
+                exit()
+
+        sendRequest(localDict)
+        localDict.clear()
     
 except Exception as e:
     print("Error : " + str(e))
